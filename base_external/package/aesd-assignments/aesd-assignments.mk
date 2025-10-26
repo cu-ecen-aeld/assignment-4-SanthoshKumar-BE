3d8806f698b1e5c85badd955d0bdcc468d03882e
SanthoshKumar-BE <santhosh29ssk@gmail.com>
2025-10-26 10:06:09 +0530

    Update aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -4,21 +4,26 @@
 #
 ################################################################################
 
-# 1. Set the source version (can be arbitrary)
+# 1. Set the source version. This is arbitrary since we use a local copy.
 AESD_ASSIGNMENTS_MAIN_VERSION = 1.0.0
 
-# 2. Set the source location to the directory above 'buildroot' (i.e., the repo root)
-# $(TOPDIR)/.. is the path to the root of the GitHub repository checkout
+# 2. CRITICAL: Define the source location as the directory *above* the Buildroot folder.
+# $(TOPDIR)/.. points to the root of your GitHub repository clone.
 AESD_ASSIGNMENTS_MAIN_SITE = $(TOPDIR)/..
 
-# 3. CRITICAL: Tell Buildroot to use the 'local' method (i.e., copy the source, do not download)
+# 3. CRITICAL: Instruct Buildroot to COPY the source code.
+# This bypasses the failed internet download entirely.
 AESD_ASSIGNMENTS_MAIN_SITE_METHOD = local
 
-# 4. Set the source code directory (e.g., if code is in a 'project' subdirectory)
-# If the source is at the repo root, you can omit this or use:
+# 4. Define the subdirectory for the source code, relative to the repository root.
+# Assuming the source code is at the root of your repository (where 'buildroot' folder is).
 AESD_ASSIGNMENTS_MAIN_SUBDIR = .
 
-# Optional: Disable hash check since it's a local copy
+# 5. Buildroot Dependencies
+AESD_ASSIGNMENTS_MAIN_DEPENDENCIES = host-make
+
+# 6. Optional: Skip checks unnecessary for local assignment code.
+AESD_ASSIGNMENTS_MAIN_SUPPORTS_INSOURCE = YES
 AESD_ASSIGNMENTS_MAIN_SKIP_LICENSE_CHECK = YES
 
 # Include the standard Buildroot package boilerplate
