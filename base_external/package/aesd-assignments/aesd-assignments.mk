30d797a9fae5776d8a9a9b5350d655f06899e42a
SanthoshKumar-BE <santhosh29ssk@gmail.com>
2025-10-26 06:14:44 +0530

    TroubleShooted File Flow

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,39 +5,40 @@
 ##############################################################
 
 AESD_ASSIGNMENTS_VERSION = main
-AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-SanthoshKumar-BE.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-SanthoshKumar-BE.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-\t$(MAKE) -C $(@D)/finder-app CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)"
+	$(MAKE) -C $(@D)/finder-app CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)"
 endef
 
-# Instalación de scripts y archivos de configuración
+# Install scripts and configuration files
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-\t# Crear directorios necesarios
-\t$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
-\t
-\t# Instalar archivos de configuración
-\tif [ -f "$(@D)/conf/username.txt" ]; then \
-\t\t$(INSTALL) -m 0644 $(@D)/conf/username.txt $(TARGET_DIR)/etc/finder-app/conf/; \
-\telse \
-\t\techo "testuser" > $(TARGET_DIR)/etc/finder-app/conf/username.txt; \
-\tfi
-\t
-\tif [ -f "$(@D)/conf/assignment.txt" ]; then \
-\t\t$(INSTALL) -m 0644 $(@D)/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/conf/; \
-\telse \
-\t\techo "assignment4" > $(TARGET_DIR)/etc/finder-app/conf/assignment.txt; \
-\tfi
-\t
-\t# Instalar binarios en /bin
-\t$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin/
-\t$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin/
-\t$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin/
-\t
-\t# Instalar scripts de test
-\t$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin/
+	# Create necessary directories
+	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
+
+	# Install configuration files
+	if [ -f "$(@D)/conf/username.txt" ]; then \
+		$(INSTALL) -m 0644 $(@D)/conf/username.txt $(TARGET_DIR)/etc/finder-app/conf/; \
+	else \
+		echo "testuser" > $(TARGET_DIR)/etc/finder-app/conf/username.txt; \
+	fi
+
+	if [ -f "$(@D)/conf/assignment.txt" ]; then \
+		$(INSTALL) -m 0644 $(@D)/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/conf/; \
+	else \
+		echo "assignment4" > $(TARGET_DIR)/etc/finder-app/conf/assignment.txt; \
+	fi
+
+	# Install binaries to /bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin/
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin/
+
+	# Install test scripts
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin/
 endef
 
 $(eval $(generic-package))
+
